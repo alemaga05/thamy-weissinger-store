2986d4f5c4ce5319aa0073d58437566edddd336e
alemaga05 <alexandremaga2020@gmail.com>
2026-09-05 13:15:32 -0300

    teste de app.js

--- src/app/app.ts
+++ src/app/app.ts
@@ -2,12 +2,12 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
 // 1. Importação dos componentes que criamos
-import { Header } from './header/header'; 
-import { Footer } from './footer/footer'; 
-import { BannerCarousel } from './banner-carousel/banner-carousel'; 
-import { Categories } from './categories/categories';
-import { PopularProducts } from './popular-products/popular-products';
-import { FavoriteFeminino } from './favorite-feminino/favorite-feminino'; 
+import { Header } from './core/header/header'; 
+import { Footer } from './core/footer/footer'; 
+import { BannerCarousel } from './pages/home/banner-carousel/banner-carousel'; 
+import { Categories } from './pages/home/categories/categories';
+import { PopularProducts } from './pages/home/popular-products/popular-products';
+import { FavoriteFeminino } from './pages/home/favorite-feminino/favorite-feminino'; 
 
 @Component({
   selector: 'app-root',
