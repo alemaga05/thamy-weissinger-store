0af5d959710d6ec0841c8833a954b0da04034304
alemaga05 <alexandremaga2020@gmail.com>
2026-09-06 18:56:06 -0300

    Atualizando os produtos da categoria.

--- src/app/pages/masculino/masculino.ts
+++ src/app/pages/masculino/masculino.ts
@@ -12,10 +12,10 @@ import { RouterModule } from '@angular/router';
 export class Masculino {
   // 1. Bolinhas de Subcategorias
   subcategories = [
-    { name: 'CAMISAS', image: '/masc-camisas.png' },
-    { name: 'SHORTS', image: '/masc-shorts.png' },
-    { name: 'REGATAS', image: '/masc-regatas.png' },
-    { name: 'CONJUNTO', image: '/masc-conjunto.png' },
+    { name: 'CAMISAS', image: '/categoriaMascCamisa.png' },
+    { name: 'SHORTS', image: '/categoriaMascShort.png' },
+    { name: 'REGATAS', image: '/categoriaMascRegata.png' },
+    { name: 'CONJUNTO', image: '/categoriaMascConjunto.png' },
     { name: 'CATEGORIA', image: '/cinza.png' },
     { name: 'CATEGORIA', image: '/cinza.png' }
   ];
@@ -26,6 +26,11 @@ export class Masculino {
     { id: 22, name: 'Short Aero Pulse', price: '89,90', image: '/prod-masc2.png' },
     { id: 23, name: 'nome do produto', price: '000,00', image: '/cinza-quadrado.png' },
     { id: 24, name: 'nome do produto', price: '000,00', image: '/cinza-quadrado.png' },
-    { id: 25, name: 'nome do produto', price: '000,00', image: '/cinza-quadrado.png' }
+    { id: 25, name: 'nome do produto', price: '000,00', image: '/cinza-quadrado.png' }, 
+    { id: 26, name: 'Camiseta Poliamida', price: '79,90', image: '/prod-masc1.png' },
+    { id: 27, name: 'Short Aero Pulse', price: '89,90', image: '/prod-masc2.png' },
+    { id: 28, name: 'nome do produto', price: '000,00', image: '/cinza-quadrado.png' },
+    { id: 29, name: 'nome do produto', price: '000,00', image: '/cinza-quadrado.png' },
+    { id: 30, name: 'nome do produto', price: '000,00', image: '/cinza-quadrado.png' }, 
   ];
 }
